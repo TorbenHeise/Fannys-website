a4a30d677a5f1e01ce423f54aa156d71df21ac9b
TorbenHeise <152693184+TorbenHeise@users.noreply.github.com>
2023-12-02 23:26:13 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -13,37 +13,38 @@ function createBlinkingLights() {
 
 // Call this function to create blinking lights when the page loads
 createBlinkingLights();
+document.addEventListener("DOMContentLoaded", function() {
+    const questions = [
+        { question: "What is the capital of France?", answer: "Paris" },
+        { question: "Which planet is known as the Red Planet?", answer: "Mars" },
+        // Add more questions as needed
+    ];
 
-const questions = [
-    { question: "What is the capital of France?", answer: "Paris" },
-    { question: "Which planet is known as the Red Planet?", answer: "Mars" },
-    // Add more questions as needed
-];
+    let currentQuestionIndex = 0;
 
-let currentQuestionIndex = 0;
-
-function displayQuestion() {
-    const questionText = document.getElementById("question-text");
-    questionText.textContent = questions[currentQuestionIndex].question;
-}
-
-function checkAnswer() {
-    const userAnswer = document.getElementById("answer-input").value;
-    const correctAnswer = questions[currentQuestionIndex].answer;
-    const resultText = document.getElementById("result");
-
-    if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
-        resultText.textContent = "Correct! Moving to the next question.";
-        currentQuestionIndex++;
-        if (currentQuestionIndex < questions.length) {
-            displayQuestion();
-        } else {
-            resultText.textContent = "Congratulations! You've completed the quiz.";
-        }
-    } else {
-        resultText.textContent = "Incorrect. Try again.";
+    function displayQuestion() {
+        const questionText = document.getElementById("question-text");
+        questionText.textContent = questions[currentQuestionIndex].question;
     }
-}
 
-// Initial setup
-displayQuestion();
+    function checkAnswer() {
+        const userAnswer = document.getElementById("answer-input").value;
+        const correctAnswer = questions[currentQuestionIndex].answer;
+        const resultText = document.getElementById("result");
+
+        if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
+            resultText.textContent = "Correct! Moving to the next question.";
+            currentQuestionIndex++;
+            if (currentQuestionIndex < questions.length) {
+                displayQuestion();
+            } else {
+                resultText.textContent = "Congratulations! You've completed the quiz.";
+            }
+        } else {
+            resultText.textContent = "Incorrect. Try again.";
+        }
+    }
+
+    // Initial setup
+    displayQuestion();
+});
