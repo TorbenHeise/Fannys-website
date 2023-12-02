a403e47e3e3226ac14501f7706ece72497a92ffd
TorbenHeise <152693184+TorbenHeise@users.noreply.github.com>
2023-12-03 00:32:40 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,47 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const questions = [
-        { question: "What is the capital of France?", answer: "Paris" },
-        { question: "Which planet is known as the Red Planet?", answer: "Mars" },
-        // Add more questions as needed
-    ];
-
-    let currentQuestionIndex = 0;
-
-    function displayQuestion() {
-        const questionText = document.getElementById("question-text");
-        questionText.textContent = questions[currentQuestionIndex].question;
-    }
-
-    function startQuiz() {
-        const startButton = document.getElementById("startButton");
-        const quizContainer = document.getElementById("quiz-container");
-        const audio = document.getElementById("myAudio");
-
-        startButton.style.display = "none";
-        quizContainer.style.display = "block";
-        audio.play();
-        displayQuestion();
-        startHeadlights();
-    }
-
-    function checkAnswer() {
-        const userAnswer = document.getElementById("answer-input").value;
-        const correctAnswer = questions[currentQuestionIndex].answer;
-        const resultText = document.getElementById("result");
-
-        if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
-            resultText.textContent = "Correct! Moving to the next question.";
-            currentQuestionIndex++;
-            if (currentQuestionIndex < questions.length) {
-                displayQuestion();
-            } else {
-                resultText.textContent = "Congratulations! You've completed the quiz.";
-            }
-        } else {
-            resultText.textContent = "Incorrect. Try again.";
-        }
-    }
-
     function startHeadlights() {
         const headlights = document.createElement("div");
         headlights.className = "headlights";
@@ -64,6 +21,5 @@ document.addEventListener("DOMContentLoaded", function() {
         setInterval(moveHeadlights, 50);
     }
 
-    window.startQuiz = startQuiz;
-    window.checkAnswer = checkAnswer;
+    window.startHeadlights = startHeadlights;
 });
