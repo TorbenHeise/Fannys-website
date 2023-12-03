0efd8ec05aeab4f2b7270a3dfb4d07f0ddde05da
TorbenHeise <152693184+TorbenHeise@users.noreply.github.com>
2023-12-03 11:53:09 +0000

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,46 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const questions = [
-        { question: "What is the capital of France?", answer: "Paris" },
-        { question: "Which planet is known as the Red Planet?", answer: "Mars" },
-        // Add more questions as needed
-    ];
-
-    let currentQuestionIndex = 0;
-
-    function displayQuestion() {
-        const questionText = document.getElementById("question-text");
-        questionText.textContent = questions[currentQuestionIndex].question;
-    }
-
-    function startQuiz() {
-        const startButton = document.getElementById("startButton");
-        const quizContainer = document.getElementById("quiz-container");
-        const audio = document.getElementById("myAudio");
-
-        startButton.style.display = "none";
-        quizContainer.style.display = "flex";
-        audio.play();
-        startHeadlights();  // Start the headlights when the quiz starts
-        displayQuestion();
-    }
-
-    function checkAnswer() {
-        const userAnswer = document.getElementById("answer-input").value;
-        const correctAnswer = questions[currentQuestionIndex].answer;
-        const resultText = document.getElementById("result");
-
-        if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
-            resultText.textContent = "Correct! Moving to the next question.";
-            currentQuestionIndex++;
-            if (currentQuestionIndex < questions.length) {
-                displayQuestion();
-            } else {
-                resultText.textContent = "Congratulations! You've completed the quiz.";
-            }
-        } else {
-            resultText.textContent = "Incorrect. Try again.";
-        }
-    }
+document.addEventListener("DOMContentLoaded", function () {
+    // ... (existing code)
 
     function startHeadlights() {
         const headlightsContainer = document.createElement("div");
@@ -75,32 +34,8 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-    window.startQuiz = startQuiz;
-    window.checkAnswer = checkAnswer;
-    document.addEventListener("DOMContentLoaded", function() {
+    window.startHeadlights = startHeadlights; // Expose startHeadlights globally
+
     // ... (existing code)
-
-    function startHeadlights() {
-        const headlightsContainer = document.createElement("div");
-        headlightsContainer.className = "headlights-container";
-        document.body.appendChild(headlightsContainer);
-
-        function createHeadlight() {
-            const headlights = document.createElement("div");
-            headlights.className = "headlights";
-            headlightsContainer.appendChild(headlights);
-
-            // ... (existing code for moving headlights)
-        }
-
-        // Create four headlights
-        for (let i = 0; i < 4; i++) {
-            createHeadlight();
-        }
-    }
-
-    window.startQuiz = startQuiz;
-    window.checkAnswer = checkAnswer;
-});
 
 });
